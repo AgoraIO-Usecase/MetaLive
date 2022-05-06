efd60028ceec7c453e6f616ea1a242c2d695f498
ZYP <zhuyuping@agora.io>
2022-05-06 18:59:37 +0800

    update key center

---
+++ iOS/App/Scene-Examples/Common/KeyCenter.swift
@@ -0,0 +1,19 @@
+//
+//  KeyCenter.swift
+//  OpenLive
+//
+//  Created by GongYuhua on 6/25/16.
+//  Copyright © 2016 Agora. All rights reserved.
+//
+
+struct KeyCenter {
+    static let appId: String = ""
+
+    // assign token to nil if you have not enabled app certificate
+    static var token: String? = nil
+    
+    /// 游戏
+    static let gameAppSecrets = ""
+    static let gameToken = "aa"
+    static let gameAppId = "10020"
+}
